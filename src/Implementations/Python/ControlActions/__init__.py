c539b3f176e5c378823efc2d780d3be4d233e4cb
SeanMcOwen <seanmcowen@gmail.com>
2024-11-12 13:16:35 -0500

    Finish food growth control action

--- src/Implementations/Python/ControlActions/__init__.py
+++ src/Implementations/Python/ControlActions/__init__.py
@@ -1,6 +1,8 @@
 from .Dummy import v1_dummy_control, v2_dummy_control
+from .Site import food_growth_control_action_v1
 
 control_action_options = {
     "DUMMY Length-1 DEF Equal Weight Option": v1_dummy_control,
     "DUMMY Length-1 DEF D Probability Option": v2_dummy_control,
+    "Food Growth Control Action V1": food_growth_control_action_v1,
 }
