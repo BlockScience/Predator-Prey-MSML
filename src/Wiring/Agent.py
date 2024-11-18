e01a305996d97815448e5753db1c507c8f48446d
SeanMcOwen <seanmcowen@gmail.com>
2024-11-17 22:54:03 -0500

    Add wiring

---
+++ src/Wiring/Agent.py
@@ -0,0 +1,15 @@
+agent_wiring = []
+
+agent_wiring.append(
+    {
+        "name": "Natural Death Wiring",
+        "components": [
+            "Natural Death Control Action",
+            "Natural Death Policy",
+            "Remove Agents Mechanism",
+        ],
+        "description": "Wiring for growth of food",
+        "constraints": [],
+        "type": "Stack",
+    }
+)
