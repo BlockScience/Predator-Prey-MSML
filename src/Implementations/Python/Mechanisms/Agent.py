e22f857f561498d8b765277be95f63c65990d40a
SeanMcOwen <seanmcowen@gmail.com>
2024-11-18 16:22:24 -0500

    Finish mechanism implementation

--- src/Implementations/Python/Mechanisms/Agent.py
+++ src/Implementations/Python/Mechanisms/Agent.py
@@ -2,3 +2,13 @@ def remove_agents_mechanism(state, params, spaces):
     state["Agents"] = [
         agent for agent in state["Agents"] if agent not in spaces[0]["Agents"]
     ]
+
+
+def update_food_mechanism(state, params, spaces):
+    for values in spaces[0]["Food Deltas"]:
+        values["Agent"]["Food"] += values["Delta Food"]
+
+
+def update_agent_age_mechanism(state, params, spaces):
+    for values in spaces[0]["Age Deltas"]:
+        values["Agent"]["Age"] += values["Delta Age"]
--- src/Implementations/Python/Policies/Agent.py
+++ src/Implementations/Python/Policies/Agent.py
@@ -12,6 +12,6 @@ def natural_death_policy(state, params, spaces):
 
 def increase_agent_age_policy_plus1(state, params, spaces):
     agents = spaces[0]["Agents"]
-    space1 = [{"Agent": x, "Delta Food": -1} for x in agents]
-    space2 = [{"Agent": x, "Delta Age": 1} for x in agents]
+    space1 = {"Food Deltas": [{"Agent": x, "Delta Food": -1} for x in agents]}
+    space2 = {"Age Deltas": [{"Agent": x, "Delta Age": 1} for x in agents]}
     return [space1, space2]
