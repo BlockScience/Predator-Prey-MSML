635aaadfa921e44f83c67e1d8e2ef35dc4435cc1
SeanMcOwen <seanmcowen@gmail.com>
2024-11-19 11:03:55 -0500

    Finish boundary action

--- src/Implementations/Python/BoundaryActions/__init__.py
+++ src/Implementations/Python/BoundaryActions/__init__.py
@@ -1,7 +1,9 @@
 from .Dummy import v1_dummy_boundary, v1_dummy_boundary2, v2_dummy_boundary2
+from .Agent import prey_feeding_boundary_action_v1
 
 boundary_action_options = {
     "Length-1 ABC Equal Weight Option": v1_dummy_boundary,
     "DUMMY Length-2 ABC Equal Weight Option": v1_dummy_boundary2,
     "DUMMY Length-2 ABC Equal Weight 2 Option": v2_dummy_boundary2,
+    "Prey Feeding Boundary Action V1": prey_feeding_boundary_action_v1,
 }
