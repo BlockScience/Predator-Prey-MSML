99e81647de7f78ebd5255d713652f46f733931f7
SeanMcOwen <seanmcowen@gmail.com>
2024-11-19 23:56:07 -0500

    Checkpoint

--- src/Implementations/Python/BoundaryActions/Agent.py
+++ src/Implementations/Python/BoundaryActions/Agent.py
@@ -2,3 +2,9 @@ def prey_feeding_boundary_action_v1(state, params, spaces):
     prey = state["Stateful Metrics"]["Prey Stateful Metric"](state, params)
     prey = [x for x in prey if x["Food"] <= params["Hunger Threshold"]]
     return [{"Agents": prey}]
+
+
+def hunt_prey_boundary_action_v1(state, params, spaces):
+    predators = state["Stateful Metrics"]["Predator Stateful Metric"](state, params)
+    predators = [x for x in predators if x["Food"] <= params["Hunger Threshold"]]
+    return [{"Agents": predators}]
