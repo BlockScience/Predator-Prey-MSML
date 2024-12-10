66b627f560db959f0b926fb811053d1aa393fcf5
SeanMcOwen <seanmcowen@gmail.com>
2024-12-10 14:46:00 -0500

    Clear dummy components

--- src/State/Global.py
+++ src/State/Global.py
@@ -3,20 +3,6 @@ global_state = {
     "notes": "",
     "variables": [
         {
-            "type": "Entity Type",
-            "name": "Dummy",
-            "description": "The dummy entity",
-            "symbol": None,
-            "domain": None,
-        },
-        {
-            "type": "DUMMY Integer Type",
-            "name": "Time",
-            "description": "The clock time",
-            "symbol": None,
-            "domain": None,
-        },
-        {
             "type": "Simulation Log Type",
             "name": "Simulation Log",
             "description": "The simulation log holding historical data",
--- src/StatefulMetrics/__init__.py
+++ src/StatefulMetrics/__init__.py
@@ -1,8 +1,6 @@
-from .Dummy import dummy_stateful_metrics
 from .Site import site_stateful_metrics
 from .Agent import agent_stateful_metrics
 
 stateful_metrics = []
-stateful_metrics.extend(dummy_stateful_metrics)
 stateful_metrics.extend(site_stateful_metrics)
 stateful_metrics.extend(agent_stateful_metrics)
