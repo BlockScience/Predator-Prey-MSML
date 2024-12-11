e08d982c6ff2afb1b09feeff9c594fc5747eafe5
SeanMcOwen <seanmcowen@gmail.com>
2024-12-10 23:50:57 -0500

    Finish removal

--- src/Implementations/Python/Mechanisms/__init__.py
+++ src/Implementations/Python/Mechanisms/__init__.py
@@ -1,8 +1,3 @@
-from .Dummy import (
-    dummy_update_dummy_entity_mechanism,
-    dummy_increment_time_mechanism,
-    dummy_log_simulation_data_mechanism,
-)
 from .Site import update_food_locations_mechanism, update_agent_locations_mechanism
 from .Agent import (
     remove_agents_mechanism,
@@ -12,10 +7,7 @@ from .Agent import (
 )
 
 mechanisms = {
-    "DUMMY Update Dummy Entity Mechanism": dummy_update_dummy_entity_mechanism,
-    "DUMMY Increment Time Mechanism": dummy_increment_time_mechanism,
-    "DUMMY Log Simulation Data Mechanism": dummy_log_simulation_data_mechanism,
-    "Update Food Locations Mechanism": update_food_locations_mechanism,
+                "Update Food Locations Mechanism": update_food_locations_mechanism,
     "Remove Agents Mechanism": remove_agents_mechanism,
     "Update Food Mechanism": update_food_mechanism,
     "Increase Agent Age Mechanism": update_agent_age_mechanism,
--- src/Implementations/Python/Metrics/Dummy.py
+++
@@ -1,2 +0,0 @@
-def dummy_multiplied_length_metric(state, params, spaces):
-    return len(spaces[0]["string"]) * params["DUMMY Length Multiplier"]
