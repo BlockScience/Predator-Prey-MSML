6afe80be8f7b988b66da6f7b95ba09a3bd050f44
SeanMcOwen <seanmcowen@gmail.com>
2024-12-16 08:13:20 -0500

    Add policy scaffold

---
+++ src/Policies/Meta.py
@@ -0,0 +1,16 @@
+simulation_meta_policy = {
+    "name": "Simulation Meta Policy",
+    "description": "The policy which determines the simulation metadata updates.",
+    "constraints": [],
+    "policy_options": [],
+    "domain": [],
+    "codomain": [
+        "Timestep Space",
+        "Simulation Log Space",
+    ],
+    "parameters_used": [],
+    "metrics_used": [],
+}
+
+
+meta_policies = [simulation_meta_policy]
