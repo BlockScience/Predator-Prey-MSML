e77a41015c6f6a0988502fb3d41ad4d3a1110e99
SeanMcOwen <seanmcowen@gmail.com>
2024-12-20 14:16:42 -0500

    Add poisson implementation

--- src/Implementations/Python/Policies/Site.py
+++ src/Implementations/Python/Policies/Site.py
@@ -1,8 +1,24 @@
+import numpy as np
+
+
 def constant_food_growth_policy(state, params, spaces):
     space = {"Food Locations": []}
     for loc in spaces[0]["Locations"]:
         delta_food = min(
             params["Maximum Food per Tile"] - loc["Food"], params["Food Growth Rate"]
+        )
+        space["Food Locations"].append(
+            {"Location": loc["Location"], "Food": delta_food}
+        )
+    return [space]
+
+
+def poisson_food_growth_policy(state, params, spaces):
+    space = {"Food Locations": []}
+    for loc in spaces[0]["Locations"]:
+        delta_food = min(
+            params["Maximum Food per Tile"] - loc["Food"],
+            np.random.poisson(["Food Growth Rate"]),
         )
         space["Food Locations"].append(
             {"Location": loc["Location"], "Food": delta_food}
