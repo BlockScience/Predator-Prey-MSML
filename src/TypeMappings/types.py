0f635f8611b90223657c3e876587ffd703054d68
SeanMcOwen <seanmcowen@gmail.com>
2025-01-15 09:28:28 -0500

    Simulation log type

--- src/TypeMappings/types.py
+++ src/TypeMappings/types.py
@@ -1,4 +1,26 @@
+from typing import List, TypedDict
+
 mapping = {
-                "EntityType": object,
-    "SimulationLogType": list,
+    "EntityType": object,
+    "SimulationLogType": List[
+        TypedDict(
+            "Simulation Log",
+            {
+                "Timestep": int,
+                "Open Locations Stateful Metric": int,
+                "Filled Locations Stateful Metric": int,
+                "Prey Locations Stateful Metric": int,
+                "Predator Locations Stateful Metric": int,
+                "Median Site Food Stateful Metric": int,
+                "Prey Stateful Metric": int,
+                "Predator Stateful Metric": int,
+                "Number of Prey Stateful Metric": int,
+                "Number of Predators Stateful Metric": int,
+                "Median Predator Food Stateful Metric": int,
+                "Median Prey Food Stateful Metric": int,
+                "Median Predator Age Stateful Metric": int,
+                "Median Prey Age Stateful Metric": int,
+            },
+        )
+    ],
 }
